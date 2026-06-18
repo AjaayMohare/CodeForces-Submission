bd9df414264867734cd28a3db1a060c7c5b7264b
ajaymohare <ajayyynot31@gmail.com>
2026-06-18 23:21:34 +0530

    create A

---
+++ Div1+Div2_2026_6_18june/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    vector<int> v(n);
+	    for(int i=0;i<n;i++){
+	        cin >> v[i];
+	    }
+	    for(int i=0;i<n;i++){
+	        for(int j=i+1;j<n;j++){
+	            if(v[i]<v[j]){
+	                v[j]=v[i];
+	                break;
+	            }
+	        }
+	    }
+	    int sum = 0;
+	    for(int i=0;i<n;i++) sum+=v[i];
+	    cout << sum << endl;
+	}
+
+}
